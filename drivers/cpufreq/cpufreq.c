66517b70d504b6fa26c9aa1bfb18ca20c4dd5775
Junjie Wu <junjiew@codeaurora.org>
2015-02-10 14:54:36 -0800

    cpufreq: Use pr_info() for driver registration and unregistration
    Many subsystems depend on cpufreq API for CPU frequency scaling.
    Cpufreq API is expected to fail until cpufreq device registers.
    
    Change pr_debug() to pr_info() so that user could determine when
    cpufreq API becomes available during boot from kernel messages. This
    is crucial to understand whether a cpufreq API failure is benign
    during early boot.
    
    Change-Id: Id2dfa009ae33859ec3efcdb29a3296e891852c6a
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2695,7 +2695,7 @@ int cpufreq_register_driver(struct cpufreq_driver *driver_data)
 		}
 	}
 
-	pr_debug("driver %s up and running\n", driver_data->name);
+	pr_info("driver %s up and running\n", driver_data->name);
 
 	return 0;
 err_if_unreg:
@@ -2727,7 +2727,7 @@ int cpufreq_unregister_driver(struct cpufreq_driver *driver)
 	if (!cpufreq_driver || (driver != cpufreq_driver))
 		return -EINVAL;
 
-	pr_debug("unregistering driver %s\n", driver->name);
+	pr_info("unregistering driver %s\n", driver->name);
 
 	subsys_interface_unregister(&cpufreq_interface);
 	if (cpufreq_boost_supported())
