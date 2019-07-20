ca2f3bb7bfc8b9a04f96e2b76bf237e5f4ad02a3
Sultan Alsawaf <sultan@kerneltoast.com>
2019-07-20 00:10:53 -0700

    kernel: Add API to mark IRQs and kthreads as performance critical
    On devices with a CPU that contains heterogeneous cores (e.g.,
    big.LITTLE), it can be beneficial to place some performance-critical
    IRQs and kthreads onto the performance CPU cluster in order to improve
    performance.
    
    This commit adds the following APIs:
    -kthread_run_perf_critical() to create and start a perf-critical kthread
    -irq_set_perf_affinity() to mark an active IRQ as perf-critical
    -IRQF_PERF_CRITICAL to schedule an IRQ and any threads it may have onto
     performance CPUs
    -PF_PERF_CRITICAL to mark a process (mainly a kthread) as performance
     critical (this is used by kthread_run_perf_critical())
    
    In order to accommodate this new API, the following changes are made:
    -Performance-critical IRQs are distributed evenly among online CPUs
     available in cpu_perf_mask
    -Performance-critical IRQs have their affinities reaffined upon exit
     from suspend (since the affinities are broken when non-boot CPUs are
     disabled)
    -Performance-critical IRQs and their threads have their affinities reset
     upon entering suspend, so that upon immediate suspend exit (when only
     the boot CPU is online), interrupts can be processed and interrupt
     threads can be scheduled onto an online CPU (otherwise we'd hit a
     kernel BUG)
    -__set_cpus_allowed_ptr() is modified to enforce a performance-critical
     kthread's affinity
    -Perf-critical IRQs are marked with IRQD_AFFINITY_MANAGED so userspace
     can't mess with their affinity
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -608,7 +608,16 @@ EXPORT_SYMBOL(cpus_up);
 
 int __ref cpu_down(unsigned int cpu)
 {
+	struct cpumask newmask;
 	int err;
+
+	cpumask_andnot(&newmask, cpu_online_mask, cpumask_of(cpu));
+
+	/* One big cluster CPU and one little cluster CPU must remain online */
+	if (!cpumask_intersects(&newmask, cpu_perf_mask) ||
+	    !cpumask_intersects(&newmask, cpu_lp_mask))
+		return -EINVAL;
+
 
 	cpu_maps_update_begin();
 
@@ -737,6 +746,7 @@ int disable_nonboot_cpus(void)
 #endif
 
 	cpu_maps_update_begin();
+	unaffine_perf_irqs();
 	first_cpu = cpumask_first(cpu_online_mask);
 	/*
 	 * We take down all of the non-boot CPUs in one shot to avoid races
@@ -828,6 +838,7 @@ void __ref enable_nonboot_cpus(void)
 	arch_enable_nonboot_cpus_end();
 
 	cpumask_clear(frozen_cpus);
+	reaffine_perf_irqs();
 out:
 	cpu_maps_update_done();
 }
