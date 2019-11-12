325d68f261e666b5afea6f09537cc45e802fe7d8
Tetsuo Osaka <tetsugit@gmail.com>
2019-11-12 09:14:14 +0100

    make the default for zswap and zram lz4 and schange it to zstd if the config for that is set

--- mm/zswap.c
+++ mm/zswap.c
@@ -95,16 +95,10 @@ static bool zswap_enabled __read_mostly = 1;
 module_param_named(enabled, zswap_enabled, bool, 0444);
 
 /* Compressor to be used by zswap (fixed at boot for now) */
-#ifdef CONFIG_CRYPTO_LZ4
-#define ZSWAP_COMPRESSOR_DEFAULT "lz4"
-#else
-#define ZSWAP_COMPRESSOR_DEFAULT "lzo"
-#endif
-
 #ifdef CONFIG_CRYPTO_ZSTD
 #define ZSWAP_COMPRESSOR_DEFAULT "zstd"
 #else
-#define ZSWAP_COMPRESSOR_DEFAULT "lzo"
+#define ZSWAP_COMPRESSOR_DEFAULT "lz4"
 #endif
 
 static char *zswap_compressor = ZSWAP_COMPRESSOR_DEFAULT;
