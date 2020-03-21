d228d0e7b9a85a00b4c71ae9e5761d0e10e5eaa0
ThunderStorms21th <pinakastorm@gmail.com>
2020-03-21 14:41:50 +0100

    selinux: enforce by default when not permissive
    * to avoid the kernel starting in permissive and breaking magisk
    
    org commit : https://github.com/ananjaser1211/Cronos_8890/commit/6972041f67d174d2462f2e7d4e7b18d16e4c82b9
    
    Thx to Anan jaser for this commit

--- security/selinux_n/selinuxfs.c
+++ security/selinux_n/selinuxfs.c
@@ -216,6 +216,8 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 
 #ifdef CONFIG_ALWAYS_PERMISSIVE
     new_value = 0;
+#else
+    new_value = 1;
 #endif
 	if (new_value != selinux_enforcing) {
 		length = task_has_security(current, SECURITY__SETENFORCE);
