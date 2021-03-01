b7d826c6fd281de30aeadbee6a614ae1704b8fc6
ThunderStorms21th <pinakastorm@gmail.com>
2021-03-01 09:38:15 +0100

    ramdisks/zips: updates initial settings and zip files to TS v4.4
    Signed-off-by: ThunderStorms21th <pinakastorm@gmail.com>

---
+++ builds/TW-PIE-alpha/ramdisk/sbin/ts-kernel.sh
@@ -0,0 +1,128 @@
+#!/system/bin/sh
+
+# ThunderStorm bash script for kernel and policy features settings v1.1
+# Thanks to MoRoGoKu and djb77
+
+# Set Variables
+BB="/sbin/busybox"
+TS_DIR="/data/.tskernel"
+LOG="$TS_DIR/tskernel.log"
+
+# Mount
+mount -t rootfs -o rw,remount rootfs;
+mount -o rw,remount /system;
+mount -o rw,remount /data;
+mount -o rw,remount /;
+
+rm -f $LOG
+
+# Create ThundeRSTormS kernel folder
+if [ ! -d $TS_DIR ]; then
+	mkdir -p $TS_DIR;
+fi
+
+echo $(date) "ThundeRSTormS-Kernel LOG" >> $LOG;
+echo " " >> $LOG;
+
+# SELinux (0 / 640 = Permissive, 1 / 644 = Enforcing)
+echo "## -- Selinux permissive" >> $LOG;
+chmod 640 /sys/fs/selinux/enforce
+echo " " >> $LOG;
+
+# SafetyNet
+echo "## -- SafetyNet permissions" >> $LOG;
+chmod 440 /sys/fs/selinux/policy
+echo " " >> $LOG;
+
+# Deepsleep fix - Tweaking logging, debugubg, tracing (@Chainfire)
+echo "## -- DeepSleep Fix" >> $LOG;
+
+dmesg -n 1 -C
+echo "N" > /sys/kernel/debug/debug_enabled
+echo "N" > /sys/kernel/debug/seclog/seclog_debug
+echo "0" > /sys/kernel/debug/tracing/tracing_on
+echo "0" > /sys/module/lowmemorykiller/parameters/debug_level
+echo "0" > /sys/module/alarm_dev/parameters/debug_mask
+echo "0" > /sys/module/binder/parameters/debug_mask
+echo "0" > /sys/module/binder_alloc/parameters/debug_mask
+echo "0" > /sys/module/powersuspend/parameters/debug_mask
+echo "0" > /sys/module/xt_qtaguid/parameters/debug_mask
+echo "0" > /sys/module/lowmemorykiller/parameters/debug_level
+echo "0" > /sys/module/kernel/parameters/initcall_debug
+
+if [ -f /data/adb/su/su.d/000000deepsleep ]; then
+	rm -f /data/adb/su/su.d/000000deepsleep
+fi
+
+for i in `ls /sys/class/scsi_disk/`; do
+	cat /sys/class/scsi_disk/$i/write_protect 2>/dev/null | grep 1 >/dev/null
+	if [ $? -eq 0 ]; then
+		echo 'temporary none' > /sys/class/scsi_disk/$i/cache_type
+	fi
+done
+echo " " >> $LOG;
+
+# Fix personalist.xml
+echo "## -- Fix Personal list" >> $LOG;
+if [ ! -f /data/system/users/0/personalist.xml ]; then
+	touch /data/system/users/0/personalist.xml;
+	chmod 600 /data/system/users/0/personalist.xml;
+	chown system:system /data/system/users/0/personalist.xml;
+fi
+
+## ThunderStormS kill Google and Media servers script
+sleep 5
+
+# Google play services wakelock fix
+echo "## -- GooglePlay wakelock fix $( date +"%d-%m-%Y %H:%M:%S" )" >> $LOG;
+	
+# FIX GOOGLE PLAY SERVICE
+su -c "pm enable com.google.android.gms/.ads.AdRequestBrokerService"
+su -c "pm enable com.google.android.gms/.ads.identifier.service.AdvertisingIdService"
+su -c "pm enable com.google.android.gms/.ads.social.GcmSchedulerWakeupService"
+su -c "pm enable com.google.android.gms/.analytics.AnalyticsService"
+su -c "pm enable com.google.android.gms/.analytics.service.PlayLogMonitorIntervalService"
+su -c "pm enable com.google.android.gms/.backup.BackupTransportService"
+su -c "pm enable com.google.android.gms/.update.SystemUpdateActivity"
+su -c "pm enable com.google.android.gms/.update.SystemUpdateService"
+su -c "pm enable com.google.android.gms/.update.SystemUpdateService\$ActiveReceiver"
+su -c "pm enable com.google.android.gms/.update.SystemUpdateService\$Receiver"
+su -c "pm enable com.google.android.gms/.update.SystemUpdateService\$SecretCodeReceiver"
+su -c "pm enable com.google.android.gms/.thunderbird.settings.ThunderbirdSettingInjectorService"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdateActivity"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdatePanoActivity"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdateService"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdateService\$Receiver"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdateService\$SecretCodeReceiver"
+echo " " >> $LOG;
+
+# init.d
+echo "## -- Remove old Init.d scripts" >> $LOG
+# remove scripts
+rm -f /system/etc/init.d/ts_swapoff.sh 2>/dev/null;
+rm -f /system/etc/init.d/feravolt_gms.sh 2>/dev/null;
+rm -f /system/etc/init.d/tskillgooogle.sh 2>/dev/null;
+rm -f /system/etc/init.d/*detach* 2>/dev/null;
+rm -rf /data/magisk_backup_* 2>/dev/null;
+
+echo "## -- Start Init.d support" >> $LOG
+if [ ! -d /system/etc/init.d ]; then
+	mkdir -p /system/etc/init.d
+fi
+chown -R root.root /system/etc/init.d
+chmod -R 755 /system/etc/init.d
+for FILE in /system/etc/init.d/*; do
+	sh $FILE >/dev/null
+	echo "## -- Executing init.d script: $FILE" >> $LOG;
+done;
+echo "## -- End Init.d support" >> $LOG;
+echo " " >> $LOG;
+
+chmod 777 $LOG;
+
+# Unmount
+mount -t rootfs -o ro,remount rootfs;
+mount -o ro,remount /system;
+mount -o rw,remount /data;
+mount -o ro,remount /;
+
--- builds/zip-aR/common/ts/initial_settings.sh
+++ builds/zip-aR/common/ts/initial_settings.sh
@@ -13,38 +13,41 @@ ui_print "@-- Add init script"
 
 # Remove another kernel files
 rm -Rf /data/.morokernel
-rm -f /system/etc/init/hw/init.moro.rc
-rm -f /system/sbin/moro_init.sh
-rm -f /system/sbin/resetprop
-rm -f /system/etc/init/hw/init.spectrum.rc
-rm -f /system/etc/init/hw/init.spectrum.sh
-rm -f /system/etc/init/hw/init.services.rc
-rm -f /system/etc/init/hw/init.ts.sh
-rm -f /system/sbin/spa
+rm -f /system/system/etc/init/hw/init.moro.rc
+rm -f /system/system/sbin/moro_init.sh
+rm -f /system/system/sbin/resetprop
+rm -f /system/system/etc/init/hw/init.spectrum.rc
+rm -f /system/system/etc/init/hw/init.spectrum.sh
+rm -f /system/system/etc/init/hw/init.services.rc
+rm -f /system/system/etc/init/hw/init.ts.sh
+rm -f /system/system/sbin/spa
 
 # cp /tmp/ts/system2/init.rc /system_root/etc/init/hw
 # chmod 750 /system_root/etc/init/hw/init.rc
 
 # Remove imported services
-sed -i '/init.moro.rc/d' /system/etc/init/hw/init.rc
-sed -i '/init.spectrum.rc/d' /system/etc/init/hw/init.rc
-sed -i '/init.ts.rc/d' /system/etc/init/hw/init.rc
-sed -i '/init.services.rc/d' /system/etc/init/hw/init.rc
+sed -i '/init.moro.rc/d' /system/system/etc/init/hw/init.rc
+sed -i '/init.spectrum.rc/d' /system/system/etc/init/hw/init.rc
+sed -i '/init.ts.rc/d' /system/system/etc/init/hw/init.rc
+sed -i '/init.services.rc/d' /system/system/etc/init/hw/init.rc
 
 # Copy kernel files
 cp /data/tmp/ts/system2/sbin/spa /data/.tskernel/initial
-cp /data/tmp/ts/system2/init.ts.rc /system/etc/init/hw
+cp /data/tmp/ts/system2/init.ts.rc /system/system/etc/init/hw
 cp /data/tmp/ts/system2/sbin/ts-kernel.sh /data/.tskernel/initial
 cp /data/tmp/ts/system2/sbin/init.spectrum.sh /data/.tskernel/initial
-cp /data/tmp/ts/system2/init.spectrum.rc /system/etc/init/hw
+cp /data/tmp/ts/system2/init.spectrum.rc /system/system/etc/init/hw
 chmod 755 /data/.tskernel/initial/spa
 chmod 755 /data/.tskernel/initial/ts-kernel.sh
 chmod 755 /data/.tskernel/initial/init.spectrum.sh
-chmod 755 /system/etc/init/hw/init.spectrum.rc
-chmod 755 /system/etc/init/hw/init.ts.rc
+chmod 755 /system/system/etc/init/hw/init.spectrum.rc
+chmod 755 /system/system/etc/init/hw/init.ts.rc
 
 # Import init.ts.rc to init.rc
-sed -i '/import \/init.${ro.zygote}.rc\/init.rc/a import \/init.ts.rc' /system/etc/init/hw/init.rc
+sed -i '/import \/init.${ro.zygote}.rc\/init.rc/a import \/init.ts.rc' /system/system/etc/init/hw/init.rc
+sed -i '/import \/system\/etc\/init\/hw\/init.${ro.zygote}.rc\/init.rc/a import \/init.ts.rc' /system/system/etc/init/hw/init.rc
+sed -i '/import \/init.${ro.zygote}.rc/a import \/init.ts.rc' /system/system/etc/init/hw/init.rc
+sed -i '/import \/system\/etc\/init\/hw\/init.${ro.zygote}.rc/a import \/system\/etc\/init\/hw\/init.ts.rc' /system/system/etc/init/hw/init.rc
 
 #Unmount
 mount -t rootfs -o ro,remount rootfs;
